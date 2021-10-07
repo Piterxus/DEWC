6dd996b289d9dc51bc16a5f85f71e59bdda6f642
Piterxus <piterxus@gmail.com>
2021-10-07 10:47:16 +0200

    Ej6 Ok

--- EjU1/Ej6.js
+++ EjU1/Ej6.js
@@ -3,29 +3,74 @@ const modulos = [
         nombre: 'Sistemas informáticos',
         curso: 1,
         alumnos: [
-            'Don Pepito', 'Perico', 'Don José'
+            'Don Pepito', 'Perico', 'Don José', 'Ximo'
         ]
     },
 
     {
         nombre: 'Desarrollo Web en entorno cliente',
+        
         curso: 2,
         asignatura: '',
         alumnos: [
-            'Juan', 'Perico', 'Andrés', 'Don Pepito'
+            'Juan', 'Perico', 'Andrés', 'Don Pepito', 'Ximo'
         ]
     },
 
 ]
 
 
-let x =     {
-    nombre: 'Sistemas informáticos',
-    curso: 1,
-    alumnos: [
-        'Don Pepito', 'Perico', 'Don José'
-    ]
-}
 console.log(" ");
-({nombre:n}=x);
-console.log(n);
+({alumnos:s}=modulos[0]);
+({alumnos:d}=modulos[1]);
+let primero = [];
+let segundo = [];
+let mySet=new Set();
+ for (i=0;i<modulos.length;i++){
+     if (modulos[i].curso===1){
+            for (j=0;j<modulos[i].alumnos.length;j++){
+                primero[j]=modulos[i].alumnos[j];
+            }
+     }
+
+     if (modulos[i].curso===2){
+        for (j=0;j<modulos[i].alumnos.length;j++){
+            segundo[j]=modulos[i].alumnos[j];
+        }
+     }
+ }
+console.log("P"+primero.length);
+console.log("S"+segundo);
+let uno=primero.length;
+let dos=segundo.length;
+
+if (uno>dos){
+    for (i=0;i<uno;i++){
+        mySet.add(primero[i]);
+    }
+}else{
+    for (i=0;i<dos;i++){
+        mySet.add(segundo[i]);
+    }
+}
+for (i=0;i<primero.length;i++){
+
+    if (mySet.has(primero[i])){
+        console.log("R"+primero[i]);
+    }
+
+}
+/*
+var mySet = new Set();
+mySet.add(modulos[0].alumnos);
+mySet.add('p')
+console.log(mySet);
+console.log(mySet.has('p'));
+
+for (i=0;i<modulos.length;i++){
+   for (j=0;j<modulos[i].alumnos.length;j++){
+    console.log(modulos[i].alumnos[j]);
+   }
+}
+
+*/
