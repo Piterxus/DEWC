5c5705449741c4bd2a7a8683573e4eec0d83eab7
Piterxus NetBeans <piterxus@gmail.com>
2021-10-12 22:28:37 +0200

    diamond ok

--- EjU1/Functions/diamond.js
+++ EjU1/Functions/diamond.js
@@ -1,53 +1,46 @@
 function diamond(n) {
-    let abc = ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    let con = ((abc.indexOf(n.toUpperCase()))) * 2;
-    let con2 = ((abc.indexOf(n.toUpperCase())));
-    let con3 = abc.indexOf(n.toUpperCase())
-    let con4 = con2;
-    let con5=con3+1;
-    let con6=con5+1;
-    
+    let abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V", "W","X","Y","Z"];
    
-    let guia = '-'.repeat(con + 1);
+    let con = abc.indexOf(n.toUpperCase());
+    let con2 = abc.indexOf(n.toUpperCase());
+    let con3 = abc.indexOf(n.toUpperCase());
+    let con4 = con2;
+
+    let con5 = con - 1;
+
+    let guia = "-".repeat(con + 1);
     let s = guia.split("");
-    //console.log(guia[1]);
+    let guiaC = "-".repeat(con - 1 + 1);
+    let sC = guiaC.split("");
 
-    s[abc.indexOf(n.toUpperCase())] = 'A';
+    s[abc.indexOf(n.toUpperCase())] = "A";
+    let b = s.toString();
 
-    console.log(s.toString());
-
-
+    console.log(b.concat("," + sC.toString()));
 
     for (i = 1; i <= con2; i++) {
         con3--;
         s[con3] = abc[i];
-        //s[con5++]=abc[i];
-        //s[con6--]="-";
-        
-        
+
+        sC[i - 1] = abc[i];
 
         s[con4--] = "-";
 
-
-        console.log(s.toString());
+        console.log(s.toString().concat("," + sC.toString()));
+        sC[i - 1] = "-";
     }
 
     for (i = 1; i <= con2; i++) {
-        let x=i;
-        con3++;
-        s[con3] = abc[i];
-        
-        s[con4++] = "-";
-        console.log(s.toString());
-    }
+        let y = sC.length;
 
-    /*
-    for(i=0;i<con;i++){
-        //console.log(guia);
-        s[i]=abc[i];
-        console.log(s.toString());
+        con3++;
+        sC[y - i - 1] = abc[con5];
+        s[con3] = abc[con5--];
+
+        s[con4++] = "-";
+        console.log(s.toString().concat("," + sC.toString()));
+        sC[y - i - 1] = "-";
     }
-    */
 }
 
-diamond('D');
+diamond("W");
