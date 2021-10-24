93565504638dc98ec7a8fdc0ac4781a9c147ebd6
Piterxus <piterxus@gmail.com>
2021-10-24 16:40:28 +0200

    Conseguido

--- EjU2/scripts2.js
+++ EjU2/scripts2.js
@@ -1,12 +1,12 @@
 let cuerpo = document.body;
 let pre = document.createElement("pre");
 let lista = document.createElement("ol");
-let lista2 = document.createElement("ol");
+
 
 
 let encabezado1 = document.getElementsByTagName("h1");
 let encabezado2 = document.getElementsByTagName("h2");
-let n = (encabezado1.length + encabezado2.length);
+
 cuerpo.appendChild(pre);
 pre.appendChild(lista);
 
@@ -15,35 +15,39 @@ for (var i = 0, max = all.length; i < max; i++) {
 
 
   for (elementos of encabezado1) {
+   
     if (all[i] == elementos) {
+     
       let a = lista.appendChild(document.createElement("li"));
-      let b = document.createElement("ol");
+      var b = document.createElement("ol");
 
 
       let text = elementos.innerHTML;
 
       a.appendChild(document.createTextNode(text));
       a.appendChild(b);
-
+    
       
     }
 
 
+
  
   }
-
   for (elementos of encabezado2) {
     if (all[i] == elementos) {
-      let a = lista.appendChild(document.createElement("li"));
-      let b = document.createElement("ol");
+     b.appendChild(document.createElement("li"));
+    
 
 
-      let text = elementos.innerHTML;
+       let text = elementos.innerHTML;
 
-      a.appendChild(document.createTextNode(text));
-      a.appendChild(b);
+      b.appendChild(document.createTextNode(text));
+    
     }
   }
+
+  
 
   
 
